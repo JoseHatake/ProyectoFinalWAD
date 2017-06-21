0dc92bec8ac4cc69e06f12b964f84463436dd25b
Jorge <billymicrosip@gmail.com>
2017-06-20 22:03:56 -0500

    Correccion de filtro

--- DUML/src/main/java/mx/ipn/escom/wad/duml/filters/FiltroSesion.java
+++ DUML/src/main/java/mx/ipn/escom/wad/duml/filters/FiltroSesion.java
@@ -36,8 +36,10 @@ public class FiltroSesion implements Filter {
 		HttpSession sesion = requestUpper.getSession();
 		
 		usuario = (Usuario) sesion.getAttribute("Usuario");
-		if (usuario != null)
+		if (usuario != null){
 			System.out.println("Usuario: " + usuario);
+			chain.doFilter(request, response);
+		}
 		else{
 			System.out.println("Redireccion al inicio...");
 			responseUpper.sendRedirect(requestUpper.getContextPath() + "/index.jsp");
